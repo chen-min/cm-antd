513818ed6124801db25120c2e444f68575428c6d
chenmin <chenmin.unicloud.com>
2024-01-11 14:23:15 +0800

    button component

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,12 +1,33 @@
-import React, { memo } from 'react'
-import type { FC, ReactNode } from 'react'
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 
-interface IProps {
-  children?: ReactNode
+import './index.scss';
+
+interface buttonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  type?: 'normal' | 'primary' | 'dashed' | 'link' | 'text';
+  size?: 'small' | 'medium' | 'large';
+  children?: ReactNode;
+  style?: React.CSSProperties;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onBlur?: React.FocusEventHandler<HTMLButtonElement>;
+  htmlType?: "button" | "submit" | "reset";
 }
 
-const Button: FC<IProps> = () => {
-  return <div>Button</div>
-}
+const Button = React.forwardRef<HTMLButtonElement, buttonProps>((props: buttonProps, ref) => {
+  const { className, type = 'normal', size = 'medium', children, style, onClick, onBlur,
+    htmlType = 'button',
+    ...others } = props;
 
-export default memo(Button)
+  const cls = classNames({
+    'ant-btn': true,
+    [`ant-btn-${size}`]: size,
+    [`ant-btn-${type}`]: type,
+    [className as string]: !!className
+  })
+  return <button {...others}
+    type={htmlType}
+    ref={ref} className={cls} style={style} onClick={onClick} onBlur={onBlur}>{children}</button>;
+})
+
+export default Button;
